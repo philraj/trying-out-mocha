5f1d43e5d7d3e86e9de0ad820830a813073ea79e
Phil Rajchgot <tophil@outlook.com>
2016-02-25 23:43:38 -0500

    adding done()

--- test/server.js
+++ test/server.js
@@ -8,11 +8,17 @@ describe("Color Code Converter API", () => {
     var url = "https://localhost:3000/rgbToHex?red=255&green=255&blue=255";
 
     it("returns status 200", () => {
-
+      request(url, (err, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      })
     });
 
     it("returns the color in hex", () => {
-
+      request(url, (err, response, body) => {
+        expect(body).to.equal('ffffff');
+        done();
+      })
     });
   });
 
@@ -21,11 +27,17 @@ describe("Color Code Converter API", () => {
     var url = "https://localhost:3000/hexToRGB?hex=00ff00";
 
     it("returns status 200", () => {
-
+      request(url, (err, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      })
     });
 
     it("returns the color in RGB", () => {
-
+      request(url, (err, response, body) => {
+        expect(body).to.equal('[0, 255, 0]');
+        done();
+      })
     });
   });
 });
