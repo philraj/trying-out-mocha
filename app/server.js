1baa9d4edfa10beb7c4a8cc27b022a0b4da4b933
Phil Rajchgot <tophil@outlook.com>
2016-02-27 20:52:35 -0500

    adding endpoint definitions to server.js

---
+++ app/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var converter = require('./converter');
+
+app.get('/rgbtohex', (req, res) => {
+
+})
+
+app.get('/hextorgb', (req, res) => {
+  
+})
